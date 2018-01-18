e255d577dd4c9d53e7aa138fbd9469bc50c998d6
Constant V <constantvallee@gmail.com>
2018-01-18 14:36:24 -0500

    Header for mobile

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,14 +1,3 @@
-function Person(name, favColor) {
-  this.name = name;
-  this.favColor = favColor;
-  this.greet = function(){
-    console.log("Hllo my name is "+ this.name + " ! and my color " + this.favColor + " !");
-  }
+import MobileMenu from './modules/MobileMenu';
 
-}
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Person("Jane Smith", "green");
-jane.greet();
+var mobileMenu = new MobileMenu();
